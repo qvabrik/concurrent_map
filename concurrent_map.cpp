0b2c3f6c754adef0311d7615b83e592541b881e2
qvabrik <kirilljkjk@yandex.ru>
2022-09-12 21:10:57 +0300

    concurrent_map v1.0

---
+++ concurrent_map.cpp
@@ -0,0 +1,166 @@
+﻿//#include <stdio.h>
+//#include <thread>
+//#include <iostream>
+//
+//constexpr size_t number_of_maps = 100;
+//
+//void WhichPart(size_t number) {
+//    std::cout << "Element " << number << " belongs to part #";
+//    std::cout << number % number_of_maps << std::endl;
+//}
+//
+//int main()
+//{
+//    for (int i = 1999; i < 2010; ++i)
+//        WhichPart(i);
+//}
+
+
+#include <algorithm>
+#include <cstdlib>
+#include <future>
+#include <map>
+#include <numeric>
+#include <random>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+#include "log_duration.h"
+#include "test_framework.h"
+
+using namespace std::string_literals;
+
+size_t NUMBER_OF_THREADS = std::thread::hardware_concurrency();
+
+template <typename Key, typename Value>
+class ConcurrentMap {
+public:
+    static_assert(std::is_integral_v<Key>, "ConcurrentMap supports only integer keys");
+
+    struct Access {
+        std::lock_guard<std::mutex> guard;
+        Value& ref_to_value;
+    };
+
+    struct Part {
+        std::mutex part_m_;
+        std::map<Key, Value> part_data_;
+    };
+
+    explicit ConcurrentMap(size_t bucket_count = NUMBER_OF_THREADS)
+        :number_of_parts_(bucket_count), data_(bucket_count)
+    {
+    }
+
+    Access operator[](const Key& key) {
+        uint64_t index = key % number_of_parts_;
+        return { std::lock_guard(data_[index].part_m_), data_[index].part_data_[key] };
+    }
+
+    std::map<Key, Value> BuildOrdinaryMap() {
+        std::map<Key, Value> result;
+
+        for (size_t i = 0; i < number_of_parts_; ++i) {
+            std::lock_guard c(data_[i].part_m_);
+            result.insert(data_[i].part_data_.begin(), data_[i].part_data_.end());
+        }
+
+        return result;
+    }
+
+private:
+    size_t number_of_parts_ = 4u;
+    std::vector<Part> data_;
+};
+
+using namespace std;
+
+void RunConcurrentUpdates(ConcurrentMap<int, int>& cm, size_t thread_count, int key_count) {
+    auto kernel = [&cm, key_count](int seed) {
+        vector<int> updates(key_count);
+        iota(begin(updates), end(updates), -key_count / 2);
+        shuffle(begin(updates), end(updates), mt19937(seed));
+
+        for (int i = 0; i < 2; ++i) {
+            for (auto key : updates) {
+                ++cm[key].ref_to_value;
+            }
+        }
+    };
+
+    vector<future<void>> futures;
+    for (size_t i = 0; i < thread_count; ++i) {
+        futures.push_back(async(kernel, i));
+    }
+}
+
+void TestConcurrentUpdate() {
+    constexpr size_t THREAD_COUNT = 3;
+    constexpr size_t KEY_COUNT = 50000;
+
+    ConcurrentMap<int, int> cm(THREAD_COUNT);
+    RunConcurrentUpdates(cm, THREAD_COUNT, KEY_COUNT);
+
+    const auto result = cm.BuildOrdinaryMap();
+    ASSERT_EQUAL(result.size(), KEY_COUNT);
+    for (auto& [k, v] : result) {
+        AssertEqual(v, 6, "Key = " + to_string(k));
+    }
+}
+
+void TestReadAndWrite() {
+    ConcurrentMap<size_t, string> cm(5);
+
+    auto updater = [&cm] {
+        for (size_t i = 0; i < 50000; ++i) {
+            cm[i].ref_to_value.push_back('a');
+        }
+    };
+    auto reader = [&cm] {
+        vector<string> result(50000);
+        for (size_t i = 0; i < result.size(); ++i) {
+            result[i] = cm[i].ref_to_value;
+        }
+        return result;
+    };
+
+    auto u1 = async(updater);
+    auto r1 = async(reader);
+    auto u2 = async(updater);
+    auto r2 = async(reader);
+
+    u1.get();
+    u2.get();
+
+    for (auto f : { &r1, &r2 }) {
+        auto result = f->get();
+        ASSERT(all_of(result.begin(), result.end(), [](const string& s) {
+            return s.empty() || s == "a" || s == "aa";
+            }));
+    }
+}
+
+void TestSpeedup() {
+    {
+        ConcurrentMap<int, int> single_lock(1);
+
+        LOG_DURATION("Single lock");
+        RunConcurrentUpdates(single_lock, 4, 50000);
+    }
+    {
+        ConcurrentMap<int, int> many_locks(100);
+
+        LOG_DURATION("100 locks");
+        RunConcurrentUpdates(many_locks, 4, 50000);
+    }
+}
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, TestConcurrentUpdate);
+    RUN_TEST(tr, TestReadAndWrite);
+    RUN_TEST(tr, TestSpeedup);
+}
